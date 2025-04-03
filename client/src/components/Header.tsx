96efd0667b99a5f7ab0442eb9db00af7e3c038cd
Victor-starr <v94625230@gmail.com>
2025-04-03 10:59:23 +0300

    feat: enhance Header component with responsive menu and theme toggle functionality

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router";
-import { useContext } from "react";
+import { Link, useLocation } from "react-router";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import useThemeToggle from "../hooks/useThemeToggle";
 import { MdSunny } from "react-icons/md";
 import { IoMdMoon } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 export default function Header() {
   const { user } = useContext(AuthContext);
   const { theme, toggleTheme } = useThemeToggle();
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location]);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   return (
     <header className="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 p-6 w-full shadow-md">
@@ -20,43 +32,57 @@ export default function Header() {
             Tasty<p className="text-yellow-500 dark:text-yellow-400">List</p>
           </Link>
         </div>
-        <ul className="flex row gap-4 font-bold text-lg items-center">
-          <li>
-            <Link to="/recipes">Recipes</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          {user ? (
-            <>
-              <li>
-                <Link to="/recipes/create">Create</Link>
-              </li>
-              <li>
-                <Link to="/auth/logout">Logout</Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/auth/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/auth/register">Register</Link>
-              </li>
-            </>
-          )}
-        </ul>
-        <button
-          onClick={toggleTheme}
-          className="p-2 rounded-full focus:outline-none"
-        >
-          {theme === "light" ? (
-            <IoMdMoon className="text-2xl" />
-          ) : (
-            <MdSunny className="text-2xl" />
-          )}
-        </button>
+
+        <div className="flex items-center gap-4">
+          <button
+            onClick={handleMenuToggle}
+            className="lg:hidden cursor-pointer text-2xl"
+          >
+            {isMenuOpen ? <IoMdClose /> : <GiHamburgerMenu />}
+          </button>
+
+          <ul
+            className={`${
+              isMenuOpen ? "flex" : "hidden"
+            } flex-col lg:flex lg:flex-row gap-4 font-bold text-lg items-center absolute lg:static top-16 left-0 w-full lg:w-auto bg-white dark:bg-gray-800 shadow-md lg:shadow-none p-6 lg:p-0 z-50`}
+          >
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/recipes/create">Create</Link>
+                </li>
+                <li>
+                  <Link to="/auth/logout">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/auth/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/auth/register">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
+          <button
+            onClick={toggleTheme}
+            className="p-2 rounded-full focus:outline-none"
+          >
+            {theme === "light" ? (
+              <IoMdMoon className="text-2xl" />
+            ) : (
+              <MdSunny className="text-2xl" />
+            )}
+          </button>
+        </div>
       </nav>
     </header>
   );
