13f76d46c5791979ada17976053c6d652bdc6ac3
Victor-starr <v94625230@gmail.com>
2025-06-02 12:09:08 +0300

    feat: Update footer component to include GitHub link and enhance layout

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,16 @@
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router";
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-200 dark:bg-gray-800 text-center p-4 mt-auto">
-      <div>
-        <p className="text-base text-gray-800 dark:text-amber-50 font-bold">
-          &copy; {new Date().getFullYear()} Your Company. All rights reserved.
-        </p>
-      </div>
+    <footer className="flex flex-row justify-center items-center gap-2 bg-gray-200 dark:bg-gray-800 mt-auto p-4 w-full font-bold text-gray-800 dark:text-amber-50 text-base text-center hover:underline">
+      <Link
+        to={"https://github.com/Victor-starr/Tasty-list"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="contents"
+      >
+        <FaGithub /> Check out the code on GitHub
+      </Link>
     </footer>
   );
 }
